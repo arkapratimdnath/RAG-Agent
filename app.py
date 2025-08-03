fa3aae5430a2eed8e283ae4a302f8fa65519414b
arkapratimdnath <arkapratimdebnath7@gmail.com>
2025-08-03 21:35:41 +0530

    updated app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def should_continue(state: AgentState):
 
 system_prompt = """
 You are an intelligent AI assistant who answers questions about various definition in insurance policy, benifits in insurance policy,locations of closest Insurance Ombudsman along with relevent links based on the PDF document loaded into your knowledge base.
-Use the retriever tool available to answer questions about the stock market performance data. You can make multiple calls if needed.
+Use the retriever tool available to answer questions about the various policy requirements . You can make multiple calls if needed.
 If you need to look up some information before asking a follow up question, you are allowed to do that!
 Please always cite the specific parts of the documents you use in your answers.
 """
